4284bdaf096c7a6488e89c2226270a803cbe0241
mac <examnagri.1@gmail.com>
2024-09-18 16:58:55 +0530

    Initial commit

--- lib/scaffold_widget.dart
+++ lib/scaffold_widget.dart
@@ -12,12 +12,62 @@ class _ScaffoldWidgetState extends State<ScaffoldWidget> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: Text('Scaffold Widget Explained'),
+        backgroundColor: Colors.grey.shade200,
+
       ),
+
+      backgroundColor: Colors.grey.shade50,
+
+      bottomNavigationBar: BottomNavigationBar(
+          items: <BottomNavigationBarItem> [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home'
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.supervised_user_circle),
+                label: 'Profile'
+            ),
+          ]
+      ),
+
+      bottomSheet: BottomSheet(
+        onClosing: () {
+
+        }, builder: (context) {
+        return Container(
+          height: 500,
+          width: double.infinity,
+          color: Colors.black87,
+        );
+      },
+      ),
+
+
+
+      drawer: Container(
+        height: double.infinity,
+        width: 200,
+        color: Colors.blueAccent,
       ),
       floatingActionButton: FloatingActionButton(
-          onPressed: () {
-          },
+        backgroundColor: Colors.blueAccent.shade700,
+
+        onPressed: () {
+
+        },
+        child: Icon(Icons.camera),
       ),
+
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text('Welcome to Flutter Course!', style: TextStyle(
+                fontSize: 25
+            ),),
+          )
         ],
       ),
     );
